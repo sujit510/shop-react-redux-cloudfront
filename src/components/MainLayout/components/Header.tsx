c31cacdbe4db947902f97785f70fa280d460b67d
Sujit Singh <sujit_singh2@epam.com>
2024-03-22 11:35:11 +0530

    Added npm script for build+deploying the app to Azure storage account and push changes to static site

--- src/components/MainLayout/components/Header.tsx
+++ src/components/MainLayout/components/Header.tsx
@@ -33,7 +33,7 @@ export default function Header() {
             underline="none"
             to="/"
           >
-            My Store!
+            My Shop!
           </Link>
         </Typography>
 
